39946b8b2acc2b693975b949d609fd50c826de36
bazaescribe <jlrosasb@gmail.com>
2015-08-30 23:24:00 -0500

    Sections added

--- res/js/menu.js
+++ res/js/menu.js
@@ -1,8 +1,22 @@
 function scrollChange(){
-  var yPos = window.scrollY;
+  var distanceY = window.scrollY;
+  var shrinkOn = 90;
+  var header = document.getElementById('menuBar');
+  var headerSize = $("#menuBar").height();
 
-  if(yPos > 100){
-      $("#menuBar").animate({ height:'45'}, 600);
+  if (distanceY > shrinkOn) {
+    //$("#menuBar").animate({ height:'50'}, 0);
+    $("#menuBar").css('background', 'rgba(255, 255, 255, 1)');
+    $("#menuBar").css('color', 'red');
+    $("#menuBar").css('box-shadow', '0px 0px 4px rgba(0, 0, 0, 0.4)');
+
+  } else {
+    if (headerSize < 61) {
+      //$("#menuBar").animate({ height:'0'}, 0);
+      //$("#menuBar").css('color', 'red');
+      $("#menuBar").css('background', 'rgba(255, 255, 255, 0)');
+      $("#menuBar").css('box-shadow', '0px 0px 4px rgba(0, 0, 0, 0)');
+    }
   }
 }
 
